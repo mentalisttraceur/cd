790ff399a7e39e93d0b30c9623f8432b2adef0f2
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2019-10-05 05:47:01 +0000

    Version number bump because last commit fixed bug

--- cd.c
+++ cd.c
@@ -23,7 +23,7 @@
 #include <unistd.h> /* execvp */
 
 
-char const version_text[] = "cd 1.0.0\n";
+char const version_text[] = "cd 1.0.1\n";
 
 char const help_text_prefix[] = "Usage: ";
 char const help_text[] =
